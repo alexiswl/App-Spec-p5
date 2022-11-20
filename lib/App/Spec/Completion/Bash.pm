73d04a552b03de729bc439d5a9fc12d5f8c26c7f
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-11-20 17:37:41 +1100

    Set IFS as local variable to prevent changing global env var

--- lib/App/Spec/Completion/Bash.pm
+++ lib/App/Spec/Completion/Bash.pm
@@ -53,8 +53,9 @@ $completion_outer
 
 _${appname}_compreply() \{
     local prefix=""
+    local IFS=\$'\\n'
     cur="\$(printf '%q' "\$cur")"
-    IFS=\$'\\n' COMPREPLY=(\$(compgen -P "\$prefix" -W "\$*" -- "\$cur"))
+    IFS=\$IFS COMPREPLY=(\$(compgen -P "\$prefix" -W "\$*" -- "\$cur"))
     __ltrim_colon_completions "\$prefix\$cur"
 
     # http://stackoverflow.com/questions/7267185/bash-autocompletion-add-description-for-possible-completions
